abcd1e67f5fb1d7149231f24e6d93031f31a3c99
jaedeokhan <hjaedeok@gmail.com>
2025-10-20 23:59:41 +0900

    refact : 기존 테스트 코드 중복되는 변수 제거

--- hhplus-tdd-java/src/test/java/io/hhplus/tdd/PointServiceTest.java
+++ hhplus-tdd-java/src/test/java/io/hhplus/tdd/PointServiceTest.java
@@ -2,7 +2,9 @@ package io.hhplus.tdd;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.PointHistory;
 import io.hhplus.tdd.point.PointService;
+import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.awt.*;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -20,36 +25,43 @@ public class PointServiceTest {
     @Mock
     private UserPointTable userPointTable;
 
+    @Mock
+    private PointHistoryTable pointHistoryTable;
+
     @InjectMocks
     private PointService pointService;
 
     @Test
     void 사용자_포인트_조회_테스트(){
         // given
-        UserPoint mockUser = new UserPoint(1L, 1000, System.currentTimeMillis());
-        when(userPointTable.selectById(1L)).thenReturn(mockUser);
+        long mockUser = 1L;
+        long mockPoint = 1000;
+        UserPoint mockUserPoint = new UserPoint(mockUser, mockPoint, System.currentTimeMillis());
+        when(userPointTable.selectById(1L)).thenReturn(mockUserPoint);
 
         // when
-        UserPoint userPoint = pointService.getPointById(1L);
+        UserPoint userPoint = pointService.getPointById(mockUser);
 
         // then
         assertThat(userPoint).isNotNull();
-        assertThat(userPoint.id()).isEqualTo(1L);
-        assertThat(userPoint.point()).isEqualTo(1000);
+        assertThat(userPoint.id()).isEqualTo(mockUser);
+        assertThat(userPoint.point()).isEqualTo(mockPoint);
     }
 
     @Test
     void 존재하지않는_사용자_포인트_조회_테스트() {
         // given
-        UserPoint mockUser = UserPoint.empty(0L);
-        when(userPointTable.selectById(0L)).thenReturn(mockUser);
+        long mockUser = 0L;
+        long mockPoint = 0;
+        UserPoint mockUserPoint = UserPoint.empty(mockUser);
+        when(userPointTable.selectById(mockUser)).thenReturn(mockUserPoint);
 
         // when
-        UserPoint userPoint = pointService.getPointById(0L);
+        UserPoint userPoint = pointService.getPointById(mockUser);
 
         // then
-            assertThat(userPoint).isNotNull();
-        assertThat(userPoint.id()).isEqualTo(0L);
-        assertThat(userPoint.point()).isEqualTo(0);
+        assertThat(userPoint).isNotNull();
+        assertThat(userPoint.id()).isEqualTo(mockUser);
+        assertThat(userPoint.point()).isEqualTo(mockPoint);
     }
 }
