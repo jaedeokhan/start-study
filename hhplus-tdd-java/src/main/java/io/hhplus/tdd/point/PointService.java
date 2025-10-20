3cb56fb59fede05af38a5cf214b4ba0ebbab46e5
jaedeokhan <hjaedeok@gmail.com>
2025-10-21 00:06:38 +0900

    feat : 포인트 내역 조회 및 성공/실패 테스크 케이스 추가

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointService.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,16 +1,25 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
 
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
     public UserPoint getPointById(long userId) {
         return userPointTable.selectById(userId);
+    }
+
+    public List<PointHistory> getHistoriesById(long userId) {
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(userId);
+        return pointHistories;
     }
 }
