b0bfd8607fcddcf2c0659b1d40da4d1d25c3fa6d
jaedeokhan <hjaedeok@gmail.com>
2025-10-26 22:48:40 +0900

    refact: 사용하지 않는 import 및 로거 제거

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointController.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,9 +1,6 @@
 package io.hhplus.tdd.point;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,12 +10,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PointController {
 
-    private static final Logger log = LoggerFactory.getLogger(PointController.class);
     private final PointService pointService;
 
-    /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
-     */
     @GetMapping("{id}")
     public UserPoint getPointById(
             @PathVariable long id
@@ -26,9 +19,6 @@ public class PointController {
         return pointService.getPointById(id);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
-     */
     @GetMapping("{id}/histories")
     public List<PointHistory> getHistoriesById(
             @PathVariable long id
@@ -36,9 +26,6 @@ public class PointController {
         return pointService.getHistoriesById(id);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
-     */
     @PatchMapping("{id}/charge")
     public UserPoint charge(
             @PathVariable long id,
@@ -46,10 +33,7 @@ public class PointController {
     ) {
         return pointService.charge(id, amount);
     }
-
-    /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-     */
+    
     @PatchMapping("{id}/use")
     public UserPoint use(
             @PathVariable long id,
