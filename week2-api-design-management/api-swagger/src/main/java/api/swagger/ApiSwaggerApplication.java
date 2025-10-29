877d55b6661e5e5d8f6d536f40512dabcb756982
jaedeokhan <hjaedeok@gmail.com>
2025-10-29 23:44:08 +0900

    chore: API 정적 스웨거 생성을 위한 프로젝트 초기 구성

--- week2-api-design-management/api-swagger/src/main/java/api/swagger/ApiSwaggerApplication.java
+++ week2-api-design-management/api-swagger/src/main/java/api/swagger/ApiSwaggerApplication.java
@@ -1,12 +1,12 @@
-package io.hhplus.tdd;
+package api.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TddApplication {
+public class ApiSwaggerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TddApplication.class, args);
+        SpringApplication.run(ApiSwaggerApplication.class, args);
     }
 }
