cdae560b1ba8df4b76bae97858b3f4795ceb581b
jaedeokhan <hjaedeok@gmail.com>
2025-11-13 23:58:33 +0900

    feat: Product Jpa repository

--- ecommerce/src/main/java/com/ecommerce/infrastructure/jpa/ProductJpaRepository.java
+++ ecommerce/src/main/java/com/ecommerce/infrastructure/jpa/ProductJpaRepository.java
@@ -1,2 +1,17 @@
-package com.ecommerce.infrastructure.jpa;public interface ProductJpaRepository {
+package com.ecommerce.infrastructure.jpa;
+
+import com.ecommerce.domain.product.Product;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id = :id")
+    Optional<Product> findByIdWithLock(@Param("id") Long id);
 }
