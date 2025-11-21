47d898ffe08f4a15d5e08d0ae8c61eff8082e9df
jaedeokhan <hjaedeok@gmail.com>
2025-11-22 01:01:56 +0900

    update: CartItemRepository 메서드 변경(findById -> findByIdOrThrow)

--- ecommerce/src/main/java/com/ecommerce/infrastructure/repository/CartRepository.java
+++ ecommerce/src/main/java/com/ecommerce/infrastructure/repository/CartRepository.java
@@ -1,6 +1,8 @@
 package com.ecommerce.infrastructure.repository;
 
 import com.ecommerce.domain.cart.CartItem;
+import com.ecommerce.domain.cart.exception.CartErrorCode;
+import com.ecommerce.domain.cart.exception.CartItemNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,6 +11,10 @@ import java.util.Optional;
 public interface CartRepository extends JpaRepository<CartItem, Long> {
 
     Optional<CartItem> findById(Long id);
+
+    default CartItem findByIdOrThrow(Long id) {
+        return findById(id).orElseThrow(() -> new CartItemNotFoundException(CartErrorCode.CART_ITEM_NOT_FOUND));
+    }
 
     List<CartItem> findByUserId(Long userId);
 
