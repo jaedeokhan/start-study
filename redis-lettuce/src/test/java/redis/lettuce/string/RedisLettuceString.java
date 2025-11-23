550891ea6b9439f6a02744b00486984611a871ce
jaedeokhan <hjaedeok@gmail.com>
2025-11-23 17:51:17 +0900

    chore: redis ttl 설정 및 del commands

--- redis-lettuce/src/test/java/redis/lettuce/string/RedisLettuceString.java
+++ redis-lettuce/src/test/java/redis/lettuce/string/RedisLettuceString.java
@@ -1,10 +1,14 @@
 package redis.lettuce.string;
 
+import io.lettuce.core.GetExArgs;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.SetArgs;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 public class RedisLettuceString {
     // string
@@ -29,6 +33,38 @@ public class RedisLettuceString {
 
         String returnValue = redisCommands.get(key);
         System.out.println("returnValue = " + returnValue);
+        System.out.println();
+
+        // TTL (time to live)
+        System.out.println("before ttl = " + redisCommands.ttl(key));
+        System.out.println();
+
+        redisCommands.expire(key, Duration.ofMinutes(1));
+        System.out.println("set expire ttl = " + redisCommands.ttl(key));
+        System.out.println();
+
+        SetArgs setArgs = SetArgs.Builder
+//                .ex(90);
+                .keepttl();
+
+        redisCommands.set(key, value + "_new", setArgs);
+        System.out.println("set SetArgs ttl = " + redisCommands.ttl(key));
+        System.out.println();
+
+        // redis 6.2 getDel, getEx
+//        String getdel = redisCommands.getdel(key);
+//        System.out.println("getdel = " + getdel);
+        System.out.println("ttl = " + redisCommands.ttl(key));
+        System.out.println();
+
+        GetExArgs getExArgs = GetExArgs.Builder.ex(120);
+        String getex = redisCommands.getex(key, getExArgs);
+        System.out.println("getex = " + getex);
+        System.out.println("ttl = " + redisCommands.ttl(key));
+
+        connection.close();
+        redisClient.shutdown();
     }
+
 
 }
